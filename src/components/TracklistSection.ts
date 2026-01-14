a48ec1762a4b5479d3d74844fd7f9d36a02e027a
JulienMnt <julien.menet@edu.devinci.fr>
2026-01-14 10:40:20 +0100

    feat: new tracklist text show

--- src/components/TracklistSection.ts
+++ src/components/TracklistSection.ts
@@ -5,15 +5,16 @@ export function createTracklistSection(): HTMLElement {
   section.className = 'w-full min-h-screen flex flex-col items-center bg-black relative';
 
   const tracks = [
-    { title: "1.L'ENNEMI", producer: 'RosalieDu38', studio: 'Studio Empire' },
-    { title: '2.LA FORGE', producer: 'Seezy', studio: 'Studio Empire' },
-    { title: "3.LA REUSSITE OU L'OUBLI", producer: 'Flem', studio: 'Studio Empire' },
-    { title: '4.UN VRAI COUTEAU', producer: 'Hologram Lo', studio: 'Studio Empire' },
-    { title: '5.OUTRO', producer: 'Kant', studio: 'Studio Empire' },
+    { title: "L'ENNEMI", producer: 'RosalieDu38', studio: 'Studio Empire' },
+    { title: 'LA FORGE', producer: 'Seezy', studio: 'Studio Empire' },
+    { title: "LA REUSSITE OU L'OUBLI", producer: 'Flem', studio: 'Studio Empire' },
+    { title: 'UN VRAI COUTEAU', producer: 'Hologram Lo', studio: 'Studio Empire' },
+    { title: 'OUTRO', producer: 'Flem', studio: 'Studio Empire' },
   ];
 
-  tracks.forEach((track) => {
-    const trackElem = createTrackSection(track.title, track.producer, track.studio);
+    tracks.forEach((track, i) => {
+    const align = i % 2 === 0 ? 'left' : 'right';
+    const trackElem = createTrackSection(track.title, track.producer, track.studio, align, i + 1);
     section.appendChild(trackElem);
   });
 

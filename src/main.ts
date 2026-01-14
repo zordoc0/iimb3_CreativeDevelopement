00bba3c8e2cda15f70569b34ed2cc225786879cd
Noa.O <noa.obringer@gmail.com>
2026-01-14 10:57:13 +0100

    fix: hero section styles and lighting, add Anton font

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { createHeader } from "./components/Header";
 import { createHeroSection } from "./components/HeroSection";
 import { createTracklistSection } from "./components/TracklistSection";
 
+
 const header = createHeader();
 const heroSection = createHeroSection();
 const tracklistSection = createTracklistSection();
-
 
 document.body.appendChild(header);
 document.body.appendChild(heroSection);
